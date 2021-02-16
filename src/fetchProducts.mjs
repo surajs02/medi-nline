b0188322a882023ec5b219af22ac322ac3d338c1
Suraj Sharma <suraj02@hotmail.co.uk>
2021-02-16 20:30:00 +0000

    Show N csv formatted products

--- src/fetchProducts.mjs
+++ src/fetchProducts.mjs
@@ -1,7 +1,9 @@
 import cheerio from 'cheerio'; // SOMEDAY: Try other parsers.
+import { delimitKeys } from './util.mjs';
 
 // Prefer fully quantified extensions.
-import { comp, first, shiftN, isIntLike, ife, axiosGetData, mapValues } from './util.mjs'; // NOTE: `.mjs`=ES (requires Node 13+) & `.js`=CommonJs.
+// NOTE: `.mjs`=ES (requires Node 13+) & `.js`=CommonJs.
+import { comp, first, shiftN, isIntLike, ife, axiosGetData, mapValues, map, join, take, filterBlankEntries } from './util.mjs'; 
 
 const tProducts = comp(first, shiftN(2))(process.argv);
 
@@ -19,6 +21,7 @@ const productClasses = {
 };
 const elToProduct = $ => el => mapValues(c => $(`.${c}`, el).text())(productClasses);
 
+// NOTE: Functions are cleaner (although this function could be even cleaner) but could use interface to support swapping parser (after adding TS).
 const getProducts = async (pageNum = 1, products = []) => {
     const $ = cheerio.load(await axiosGetData(MEDINO_POPULAR_URL + pageNum));
     products = $('.product-list-item').toArray().map(elToProduct($));
@@ -28,7 +31,10 @@ const getProducts = async (pageNum = 1, products = []) => {
 };
 
 ife(async () => {
-    const [products] = await getProducts();
+    const [products, pageNum] = await getProducts();
 
-    console.debug('products', products);
+    console.info(`Got ${products.length} products from ${pageNum} pages, showing ${tProducts} products:\n`);
+
+    console.info(`${comp(delimitKeys(), first)(products)}`);
+    console.info(comp(join('\n'), map(filterBlankEntries), take(tProducts))(products));
 });
--- src/util.mjs
+++ src/util.mjs
@@ -1,25 +1,61 @@
 // Prefer ES6 imports as cleaner.
 
+// SOMEDAY: These functions would be better with typing.
+
 import axios from 'axios';
 import cheerio from 'cheerio';
 
 export const identity = v => v;
-export const noop = () => null; // Prefer null for nil conditionals.
+export const tauto = () => true;
+export const contra = () => false;
+export const noop = () => null; // Prefer `null` for nil conditionals.
+export const isNil = v => v == null;
+export const isEmpty = v => {
+    switch (typeof v) {
+    case 'object':
+        return v == null
+            ? false
+            : Array.isArray(v)
+                ? v.length < 1
+                : Object.keys(v).length < 1;
+    case 'string':
+        return v.length < 1;
+    default:
+        return false;
+    }
+};
+export const isBlankStr = (s = '') => s.trim().length < 1;
 
 export const comp = (...fs) => x => fs.reverse().reduce((a, v) => v(a), x);
 export const map = (t = identity) => (a = []) => a.map(t);
+export const filter = (p = tauto) => (a = []) => a.filter(p);
 export const first = (a = []) => a[0];
 export const tail = ([, ...a]) => a;
-export const negate = (p = noop) => (...a) => !p(a);
+export const negate = (p = noop) => (...a) => !p(...a);
+// NOTE: Eslint doesn't understand recursion.
 // eslint-disable-next-line no-unused-vars
-export const shiftN = n => (a = []) => n < 1 ? a : shiftN(--n)(tail(a));
-
+export const shiftN = n => (a = []) => n < 1 ? a : shiftN(--n)(tail(a)); // DEPRECATED over `sliceN` but still a good ref.
+export const sliceN = n => (a = []) => a.slice(n);
+export const take = n => (a = []) => a.slice(0, n);
 export const range = (end = 10, start = 0) => [...Array(end).keys()].map(n => n + start);
+export const join = (delimiter = ',') => (a = []) => a.join(delimiter);
 
 export const keys = (o = {}) => Object.keys(o);
 export const values = (o = {}) => Object.values(o);
 export const mapValues = (t = identity) => (o = {}) => keys(o).reduce((a, k) => ({ ...a, [k]: t(o[k], k) }), {});
+export const mapEntries = ({ setValue = identity, setKey = (__, k) => k, keep = tauto } = {}) => (o = {}) =>
+    keys(o).reduce(
+        (a, k) => keep(o[k], k, a)
+            ? ({ ...a, [setKey(o[k], k, a)]: setValue(o[k], k, a) })
+            : a,
+        {}
+    );
+export const filterEntries = (p = tauto) => (o = {}) => mapEntries({ keep: p })(o);
+export const filterBlankEntries = (o = {}) => comp(delimitValues(), filterEntries(negate(isBlankStr)))(o);
 export const grab = k => (o = {}) => o[k];
+export const reduceEntries = (t, init = []) => (o = {}) => keys(o).reduce((a, k) => t(o[k], k, a), init);
+export const delimitKeys = (delimiter = ',') => (o = {}) => reduceEntries((__, k, a) => a.concat(k))(o).join(delimiter);
+export const delimitValues = (delimiter = ',') => (o = {}) => reduceEntries((v, __, a) => a.concat(v))(o).join(delimiter);
 
 // Prefer `ife` aka IIFE to reduce parentheses.
 export const ife = (f = noop, ...a) => (() => f(...a))();
